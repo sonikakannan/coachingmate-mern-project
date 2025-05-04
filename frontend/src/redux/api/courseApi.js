a363ede09e92fb611a8255d31be74239d6e18ee5
sonikakannan <148752709+sonikakannan@users.noreply.github.com>
2025-05-04 20:18:26 +0530

    Update courseApi.js

--- frontend/src/redux/api/courseApi.js
+++ frontend/src/redux/api/courseApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const BASE_URL = "https://coachingmate-backend.onrender.com/api/generate"
+const BASE_URL = import.meta.env.MODE === "development"?"http://localhost:5001/api/generate":"/api/generate"
 
 
 export const courseApi = createApi({
