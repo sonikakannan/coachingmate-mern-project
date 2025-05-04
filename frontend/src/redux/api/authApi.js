0bbf0a96cd392c7219a3c786d3f21831e371de2a
sonikakannan <148752709+sonikakannan@users.noreply.github.com>
2025-05-04 20:01:02 +0530

    Update authApi.js

--- frontend/src/redux/api/authApi.js
+++ frontend/src/redux/api/authApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const BASE_URL = "https://coachingmate.onrender.com/api/auth"
+const BASE_URL = "https://coachingmate-backend.onrender.com/api/auth"
 
 export const authApi = createApi({
   reducerPath: 'authApi',
