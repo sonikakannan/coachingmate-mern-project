f3fe3acc9f74184022fbae621975f7dd8f05debd
sonikakannan <sonikakannan66@gmail.com>
2025-05-04 18:44:37 +0530

    first commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,13 +28,14 @@ app.use("/api/auth", authRoutes)
 app.use("/api/generate", generateTopicsRoute)
 app.use("/api/stripe", stripeRoutes);
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
     app.get("/*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
     });
-    
 }
+
 app.listen(PORT,()=>{
     console.log(`Server is running on PORT ${PORT}`);
     connectDB()
