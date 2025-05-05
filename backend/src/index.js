fbeace39ae6f71072ede90a085dc9a36b070c7e2
sonikakannan <148752709+sonikakannan@users.noreply.github.com>
2025-05-05 08:48:39 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,13 +27,10 @@ app.use("/api/auth", authRoutes)
 app.use("/api/generate", generateTopicsRoute)
 app.use("/api/stripe", stripeRoutes);
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+app.get("*", (req, res) => {
+   res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+ });
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-      });
-}
 
 app.get('/', (req, res) => {
   res.send('API is working!');
