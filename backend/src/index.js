6627cbd34fcea4e5765bde452db4daaa1be4a071
sonikakannan <148752709+sonikakannan@users.noreply.github.com>
2025-05-05 08:54:49 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,6 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import path from 'path';
-
 import authRoutes from './routes/auth.route.js';
 import { connectDB } from './config/db.js';
 import generateTopicsRoute from './routes/course.route.js';
@@ -13,7 +11,6 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve(); 
 
 // Middleware
 app.use(express.json());
@@ -32,16 +29,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/generate", generateTopicsRoute);
 app.use("/api/stripe", stripeRoutes);
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
 // Health check route
 app.get('/', (req, res) => {
   res.send('API is working!');
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
